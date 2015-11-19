d224d47f6368c9ce02233d5f13e2cd684f3e031e
Danny Somekh <somekh.daniel@gmail.com>
2015-11-19 14:45:56 -0500

    number property deleted on steps model

--- server/db/models/step.js
+++ server/db/models/step.js
@@ -9,7 +9,6 @@ var objectId = mongoose.Schema.Types.ObjectId;
 var schema = new mongoose.Schema({
 	name: {type: String, required: true},
 	requirements: [{type: requirementSchema, minLength: 1}],
-	number: {type: Number, default: 1},
 	media: {type: objectId, ref: 'Media', required: true},
 	description: {type: String, required: true, maxLength:200},
 	help: {type: objectId, ref: 'Help'},
