fc7253467c80050cb64e8d666b57deb93a43e404
Danny Somekh <somekh.daniel@gmail.com>
2015-11-20 12:21:03 -0500

    Seed changed to add more tutorials and another user

--- seed.js
+++ seed.js
@@ -30,25 +30,90 @@ var Media = mongoose.model('Media');
 
 
 var seedUsers = function () {
-
-    var users = {
-        email: 'testing@fsa.com',
-        password: 'password',
-        firstName: 'Omri',
-        lastName: 'Bernstein' 
-    }
+    var users = [
+        {
+            email: 'testing@fsa.com',
+            password: 'password',
+            firstName: 'Omri',
+            lastName: 'Bernstein' 
+        },
+        {
+            email: 'marth@fsa.com',
+            password: 'password',
+            firstName: 'Martha',
+            lastName: 'Stewart' 
+        }
+    ]
     return User.create(users);
 
 };
 
-var seedTutorials = function(userId, categoryId){
-    var tutorial = {
-        name: 'Brownies',
-        description: 'Yumpp brownies description',
-        quantity: 3,
-        author: userId,
-        category: categoryId
-    }
+var seedTutorials = function(userId, categoryId, stepId){
+    var tutorial = [
+        {
+            name: 'Brownies',
+            description: 'This is a tutorial for the best brownies ever. They have a lot of chocolate.',
+            quantity: 3,
+            author: userId,
+            category: categoryId,
+            requirements: [
+                {
+                    quantity: 3,
+                    unit: 'cups',
+                    item: 'Sugar'
+                },
+                {
+                    quantity: 3,
+                    unit: 'feet',
+                    item: 'Eggs'
+                }
+            ],
+            steps: [stepId],
+            equipment: ['Oven', 'Mixer']
+        },
+        {
+            name: 'Brownies',
+            description: 'This is a tutorial for the best brownies ever. They have a lot of chocolate.',
+            quantity: 3,
+            author: userId,
+            category: categoryId,
+            requirements: [
+                {
+                    quantity: 3,
+                    unit: 'cups',
+                    item: 'Sugar'
+                },
+                {
+                    quantity: 3,
+                    unit: 'feet',
+                    item: 'Eggs'
+                }
+            ],
+            steps: [stepId],
+            equipment: ['Oven', 'Mixer']
+        },
+        {
+            name: 'Brownies',
+            description: 'This is a tutorial for the best brownies ever. They have a lot of chocolate.',
+            quantity: 3,
+            author: userId,
+            category: categoryId,
+            requirements: [
+                {
+                    quantity: 3,
+                    unit: 'cups',
+                    item: 'Sugar'
+                },
+                {
+                    quantity: 3,
+                    unit: 'feet',
+                    item: 'Eggs'
+                }
+            ],
+            steps: [stepId],
+            equipment: ['Oven', 'Mixer']
+        }
+    ]
     return Tutorial.create(tutorial)
 }
 
@@ -91,15 +156,20 @@ var seedSteps = function(mediaId){
 }
 
 connectToDb.then(function () {
+    var user;
+    var category;
     mongoose.connection.db.dropDatabase()
     .then(function(){
         return Promise.all([seedUsers(), seedCategories(), seedMedias()])
     })
     .then(function(values){
-        var user = values[0];
-        var media = values[2];
-        var category = values[1]
-        return Promise.join(seedTutorials(user._id, category._id), seedSteps(media._id))
+        user = values[0][0];
+        media = values[2];
+        category = values[1]
+        return seedSteps(media._id)
+    })
+    .then(function(step){
+        return seedTutorials(user._id, category._id, step._id)
     })
     .then(function () {
         console.log(chalk.green('Seed successful!'));
