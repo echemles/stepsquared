46b066c1d2013135fafd4539a6f1a815fcfec6fe
Danny Somekh <somekh.daniel@gmail.com>
2015-11-20 09:04:03 -0500

    helper function to send back response

--- browser/js/common/factories/CategoryFactory.js
+++ browser/js/common/factories/CategoryFactory.js
@@ -2,32 +2,30 @@ app.factory('CategoryFactory', function($http){
 
 	var CategoryFactory = {};
 
+	function getData(response){
+		return response.data;
+	}
+
 	CategoryFactory.getOne = function(categoryId){
-		return $http.get('/api/categories/' + categoriesId)
-		.then(function(res){
-			return res.data
-		})
+		return $http.get('/api/categories/' + categoryId)
+		.then(getData)
 	}
 
 	CategoryFactory.delete = function(categoryId) {
-		return $http.delete('/api/categories/'+categoryId)
-		.then(function(res){
+		return $http.delete('/api/categories/' + categoryId)
+		.then(function(){
 			return "categories successfully deleted"
 		})
 	}
 
 	CategoryFactory.create = function(category){
 		return $http.post('/api/categories/', category)
-		.then (function(res){
-			return res.data;
-		})
+		.then(getData)
 	}
 
 	CategoryFactory.update = function(category){
 		return $http.put('/api/categories/'+ category._id, category)
-		.then (function(res){
-			return res.data;
-		})
+		.then(getData)
 	}
 
 	return CategoryFactory;
