01700a4b37b7c8da861ab0d1bce4895c1486df4e
Danny Somekh <somekh.daniel@gmail.com>
2015-11-23 18:03:06 -0500

    deleted bower stuff

--- bower_components/ng-chance/ng-chance.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-angular.module('ngChance')
-  .directive('ngChance', function() {
-    return function(scope, element, attrs) {
-    	element.text(chance[attrs.ngChance]);
-    }
-  });
