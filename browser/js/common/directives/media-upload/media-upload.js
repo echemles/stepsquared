f83aae8b59ff15b93e3ca818b07595a001f14fba
Danny Somekh <somekh.daniel@gmail.com>
2015-11-24 09:41:49 -0500

    Upload code mvoed to directive

--- browser/js/common/directives/media-upload/media-upload.js
+++ browser/js/common/directives/media-upload/media-upload.js
@@ -1,4 +1,4 @@
-app.directive('mediaUpload', function(){
+app.directive('mediaUpload', function(UploadFactory){
 	return {
 		restrict: 'E',
 		scope: {
@@ -9,18 +9,7 @@ app.directive('mediaUpload', function(){
 		templateUrl: 'js/common/directives/media-upload/media-upload.html',
 		link: function(scope){
 			scope.media = scope.media ? scope.media : {};
-			scope.upload = function(file){
 
-				// return url;
-				scope.isUploading.isUploading = true;
-				// scope.media.push({url: url})
-				// MediaFactory.upload(file)
-				// .then(function(){
-				// 	scope.isUploading.isUploading = false;
-					// scope.media = 
-					
-				// })
-			}
 			scope.getTypes = function(){
 				var res = [];
 				scope.types.forEach(function(type){
@@ -28,6 +17,39 @@ app.directive('mediaUpload', function(){
 				})
 				return res.join(' ')
 			}
+
+			var newMedia = document.querySelector("#uploadMedia");
+
+			function upload_file(file, signed_request, url){
+			    var xhr = new XMLHttpRequest();
+			    xhr.open("PUT", signed_request);
+			    xhr.setRequestHeader('x-amz-acl', 'public-read');
+			    xhr.onload = function(data) {
+			    	scope.isUploading.isUploading = false;
+			        if (xhr.status === 200) {
+			            console.log("Uploaded file: ", data)
+			            scope.media = data;
+			            console.log("set the current things media to the returned url")
+			        }
+			    };
+			    xhr.onerror = function(err) {
+			    	scope.isUploading.isUploading = false;
+			        console.log("ERROR: ", err)
+			    };
+			    xhr.send(file);
+			}
+
+			newMedia.onchange = function(event){
+				// var bucket = new AWS.S3({params: {Bucket: 'trikshot'}});
+				scope.isUploading.isUploading = true;
+				var media = event.target.files[0];
+				UploadFactory.uploadMedia(media)
+				.then(function(signedURL){
+					console.log("signedURL is ", signedURL)
+					upload_file(media, signedURL.signed_request, signedURL.url)
+				})
+			}
+
 		}
 	}
 })
